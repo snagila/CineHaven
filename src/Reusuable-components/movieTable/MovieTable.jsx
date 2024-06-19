e221df9bb7875a3231c13a02a0a6afcc31d85d0b
Sachin Nagila <sachinnagila2053@gmail.com>
2024-06-19 21:26:55 +1000

    mylist table component made

--- src/Reusuable-components/movieTable/MovieTable.jsx
+++ src/Reusuable-components/movieTable/MovieTable.jsx
@@ -6,16 +6,22 @@ import pic from "../../assets/hero.png";
 const MovieTable = () => {
   return (
     <>
-      {" "}
       <Header />
       <div className="mainTable">
         <div className="title container">
           <h2>All Movies</h2>
+          <hr />
         </div>
-        <div className="movieTable">
+        <div className="movieTable container">
           <div className="movieCard">
             <div className="image">
               <img src={pic} alt="" className="tableImage" />
+            </div>
+            <div className="movieCardDescription p-2">
+              <h3>Movie title</h3>
+            </div>
+            <div className="button d-grid p-1">
+              <button className="btn btn-danger  ">Delete</button>
             </div>
           </div>
         </div>
